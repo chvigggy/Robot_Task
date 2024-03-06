afa6c62a6956820699708313799daa0e0f4a76bb
chigurupati v naga sai vignesh <nagasai.chigurupati@bazaarvoice.com>
2024-03-06 10:14:11 +0530

    create a function to handle the direction string

---
+++ Robot.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Robot {
+    static void finalPosition(String p) {
+       System.out.println("hi");
+    }
+
+    public static void main(String[] args) {
+        String p = "UDDLRL";
+        finalPosition(p);
+    }
+}
+
