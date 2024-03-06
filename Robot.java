c11080009cbba562c16a2a1a6ed6c20b17fce9a8
chigurupati v naga sai vignesh <nagasai.chigurupati@bazaarvoice.com>
2024-03-06 11:02:05 +0530

    create a map and put the mappings

--- Robot.java
+++ Robot.java
@@ -3,8 +3,16 @@ import java.util.HashMap;
 
 class Robot {
     static void finalPosition(String p) {
-       System.out.println("hi");
+        Map<Character, Integer> dm = new HashMap<>();
+        dm.put('U', 0);
+        dm.put('D', 0);
+        dm.put('L', 0);
+        dm.put('R', 0);
+
     }
+
+    
+    
 
     public static void main(String[] args) {
         String p = "UDDLRL";
